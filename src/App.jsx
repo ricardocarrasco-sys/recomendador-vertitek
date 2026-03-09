01e5760b0b734fbbe9984cd388087350e414160a
ricardocarrasco-sys <ricardocarrasco@grupovertikal.cl>
2026-03-09 17:33:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -927,6 +927,8 @@ export default function App() {
             ) : null}
           </div> 
           
+          </div>          
+         
           <div className="card">
           <div style={{ fontWeight: 800 }}>Top sugerido</div>
           <div className="small" style={{ marginTop: 8 }}>
